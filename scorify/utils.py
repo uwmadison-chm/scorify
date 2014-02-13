efe645bccbc5718f48d90a5e70c61d364c151527
Nate Vack <njvack@wisc.edu>
2014-02-13 15:40:05 -0600

    Add pretty-printing fx

---
+++ scorify/utils.py
@@ -0,0 +1,28 @@
+# coding: utf8
+# Part of the scorify package
+# Copyright 2014 Board of Regents of the University of Wisconsin System
+
+import re
+import math
+
+float_stripper = re.compile(r"\.0*$")
+
+
+def pp(val, float_places=2, none_val='NaN'):
+    mapping = {
+        float: float_pp
+    }
+    mapper = mapping.get(type(val))
+    if mapper:
+        return mapper(val, float_places, none_val)
+    else:
+        if val is None:
+            return none_val
+    return str(val)
+
+
+def float_pp(num, float_places=2, none_val='NaN'):
+    if math.isnan(num) or math.isinf(num):
+        return none_val
+    rounded = str(round(num, float_places))
+    return float_stripper.sub("", rounded)
---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+# coding: utf8
+# Part of the scorify package
+# Copyright 2014 Board of Regents of the University of Wisconsin System
+
+import pytest
+
+from scorify import utils
+
+def test_float_pp():
+    assert utils.float_pp(1.0) == "1"
+    assert utils.float_pp(float('nan'), none_val='NaN') == "NaN"
+    assert utils.float_pp(1.2345, float_places=2) == "1.23"
+
+
+def test_pp():
+    assert utils.pp(1.0) == "1"
+    assert utils.pp(None, none_val='NaN') == 'NaN'
+    assert utils.pp(True) == 'True'
