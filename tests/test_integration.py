ede3fb905e0d9c4d1c21a7361eb6681eedf3084b
Nate Vack <njvack@wisc.edu>
2020-10-09 21:25:21 +0000

    Improve formatting in test_integration.py

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,15 +1,11 @@
 from __future__ import absolute_import, division
 
 import pytest
-import os
-import openpyxl
 import csv
-import warnings
 
-from .base import *
-from scorify.score_data import main_test
-from scorify.scoresheet import SectionError
-from scorify.aggregators import AggregatorError
+from .base import from_subdir
+from scorify.scripts.score_data import main_test
+
 
 def run_score_data(scoresheet, data, output, sheet=None):
     args = [
@@ -21,12 +17,14 @@ def run_score_data(scoresheet, data, output, sheet=None):
         args.append("--sheet={0}".format(sheet))
     main_test(args)
 
+
 def run_test(scoresheet, data, expected):
     run_score_data(scoresheet, data, expected)
 
     expected_content = open(from_subdir("input", expected)).read()
     actual_content = open(from_subdir("output", expected)).read()
     assert actual_content == expected_content
+
 
 def run_excel_test(scoresheet, data, expected, sheet=None):
     run_score_data(scoresheet, data, expected, sheet=sheet)
@@ -45,27 +43,36 @@ def run_excel_test(scoresheet, data, expected, sheet=None):
     assert elist == alist
 
 
-
 def test_basic_integration():
     run_test("001_scoresheet.csv", "001_data.csv", "001_expected.csv")
 
+
 def test_scoresheet_error():
     with pytest.raises(SystemExit) as e:
-        run_test("002_broken_scoresheet.csv", "001_data.csv", "001_expected.csv")
+        run_test(
+            "002_broken_scoresheet.csv", "001_data.csv", "001_expected.csv")
         assert e.value.code == 1
 
+
 def test_excel_integration():
-    run_excel_test("003_scoresheet.xlsx", "003_data.xlsx", "003_expected.csv")
+    run_excel_test(
+        "003_scoresheet.xlsx", "003_data.xlsx", "003_expected.csv")
+
 
 def test_excel_specific_sheet():
-    run_excel_test("003_scoresheet.xlsx", "003_data.xlsx", "004_expected.csv", 1)
+    run_excel_test(
+        "003_scoresheet.xlsx", "003_data.xlsx", "004_expected.csv", 1)
+
 
 def test_excel_integration_error():
     pytest.skip
-    # Need to confirm that input with errors gets correctly handled in excel land
+    # Need to confirm that input with errors gets correctly handled in excel
+    # land
     with pytest.raises(SystemExit) as e:
-        run_excel_test("004_broken_scoresheet.xlsx", "003_data.xlsx", "003_expected.csv")
+        run_excel_test(
+            "004_broken_scoresheet.xlsx", "003_data.xlsx", "003_expected.csv")
         assert e.value.code == 1
+
 
 def test_score_column_names():
     run_test("005_scoresheet.csv", "005_data.csv", "005_expected.csv")
