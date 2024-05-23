b129cb87d3c0badf122a26f019cdac8dad1f7310
Nate Vack <njvack@wisc.edu>
2024-05-23 13:42:45 -0500

    Change to calendar versioning, bump version
    I think a "how old is this" is more useful than somewhat arbitrarily
    choosing major versions

--- scorify/info.py
+++ scorify/info.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-version = __VERSION__ = "0.9.3"
+version = __VERSION__ = "2024.05.23"
 author = "Nathan Vack"
 author_email = "njvack@wisc.edu"
 license = "MIT"
-copyright = "Copyright 2020 Boards of Regent of the University of Wisconsin System"
+copyright = "Copyright 2024 Boards of Regent of the University of Wisconsin System"
 url = "https://github.com/uwmadison-chm/scorify"
